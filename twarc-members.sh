faf225b5a56eca2210da4ab676004ccedceafdb3
Markus Reiter-Haas <iseratho@gmail.com>
2022-07-25 11:34:40 +0200

    Add ids extraction

--- twarc-members.sh
+++ twarc-members.sh
@@ -1,1 +1,3 @@
-twarc2 lists members 1206514030912901120 > mps.json
+twarc2 lists members 1206514030912901120 mps.json
+twarc2 flatten mps.json mps_flat.json
+twarc2 dehydrate --id-type users mps.json mp_ids.txt
